c0219da6fe8ae9d47a113254703e034fb234beda
ssakthivel02 <ssakthivel02@gmail.com>
2026-09-03 00:16:45 +0100

    Load RamaVerse mobile navigation polish

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./experience.css";
+import "./mobile-polish.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
