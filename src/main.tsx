f79bb7bf3bb10cb0f5b9c2f655ee66b16f96f33a
ssakthivel02 <ssakthivel02@gmail.com>
2026-09-02 21:53:59 +0100

    Wire route experience styling and production service worker

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import "./experience.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+
+if (import.meta.env.PROD && "serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    void navigator.serviceWorker.register("/sw.js");
+  });
+}
